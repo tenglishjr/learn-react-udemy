57301a36e7d319ddb695a73aa68482fb9ef6ffeb
tenglishjr <timothy.english.jr@gmail.com>
2022-04-27 00:44:30 -0500

    Communicating with Props

--- src/index.js
+++ src/index.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentDetail from "./components/CommentDetail";
+import faker from "@faker-js/faker";
+import ApprovalCard from "./components/ApprovalCard";
 
-// Create a React component
 const App = () => {
-    const buttonText = { text: 'Click Me!' };
-    const labelText = 'Enter name:';
-
     return (
-        <div>
-            <label className='label' htmlFor="name">
-                {labelText}
-            </label>
-            <input type="text" id='name'/>
-            <button style={{ backgroundColor: 'blue', color: 'white' }}>
-                {buttonText.text}
-            </button>
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <div>
+                    <h4>WARNING!</h4>
+                    Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeAgo='Today at 5:00PM'
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeAgo='Today at 5:00PM'
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author={faker.name.firstName()}
+                    timeAgo='Today at 5:00PM'
+                    content={faker.lorem.sentence()}
+                />
+            </ApprovalCard>
         </div>
     );
 };
 
-// Render the React component in the DOM
 ReactDOM.render(
     <App/>,
     document.querySelector('#root')
